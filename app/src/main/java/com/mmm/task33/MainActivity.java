12e8701e1981ed749a1f705ea73aeb33b8d49207
monul <monu.lakhara2004@gmail.com>
2023-03-14 23:10:50 +0530

    commit

--- app/src/main/java/com/mmm/task33/MainActivity.java
+++ app/src/main/java/com/mmm/task33/MainActivity.java
@@ -2,13 +2,74 @@ package com.mmm.task33;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView facebook,snapchat,youtube,instagram,whatsapp;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        facebook = findViewById(R.id.facebook);
+        snapchat = findViewById(R.id.snapchat);
+        youtube = findViewById(R.id.youtube);
+        instagram = findViewById(R.id.instagram);
+        whatsapp = findViewById(R.id.whatsapp);
+
+        facebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.facebook.com"));
+                startActivity(intent);
+            }
+        });
+snapchat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.snapchat.com"));
+                startActivity(intent);
+            }
+        });
+youtube.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.youtube.com"));
+                startActivity(intent);
+            }
+        });
+instagram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.instagram.com"));
+                startActivity(intent);
+            }
+        });
+whatsapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.whatsapp.com"));
+                startActivity(intent);
+            }
+        });
+
     }
 }
